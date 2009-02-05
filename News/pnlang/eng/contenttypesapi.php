9795855e43a7796e042a9967abfd3a9eeb7e3742
Erik Spaan <erik.spaan@gmail.com>
2009-02-05 16:49:39 +0000

    [newsarticles] Updated to storiesext 2.5 (multiple categories/properties, new order choice, topicimage/path)

--- News/pnlang/eng/contenttypesapi.php
+++ News/pnlang/eng/contenttypesapi.php
@@ -10,46 +10,54 @@
  * @package Zikula_Value_Addons
  * @subpackage News
 */
-define('_NEWS_CONTENTTYPE_NEWSARTICLESTITLE', 'Recent News Articles');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESDESCR', 'Shows a specific number of news articles from one or all the categories available');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESBASICINFO', 'Basic information');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESEXTRAINFO', 'Extra information');
 define('_NEWS_CONTENTTYPE_ARCHIVED', 'Archived');
-define('_NEWS_CONTENTTYPE_PENDING', 'Pending');
-define('_NEWS_CONTENTTYPE_PUBLISHEDSTATUS', 'Status');
-define('_NEWS_CONTENTTYPE_PUBLISHED', 'Published');
-define('_NEWS_CONTENTTYPE_REJECTED', 'Rejected');
-define('_NEWS_CONTENTTYPE_UNKNOWN', 'Unknown');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESOFTHECAT', 'News articles list of the "%category%" category');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESWIDGETTITLE', 'Widget title');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESEMPTYMSG', 'No recent news articles found.');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESSHOW', 'Show');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESFRONTPAGE', 'Front page');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESNONFRONTPAGE', 'Non front page');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESLIMIT', 'Number of articles');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESMAXDAYS', 'Days limit (0 no limit)');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESMAXTITLELENGTH', 'Max. lenght of each title (0 no limit)');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESTITLEWRAPTEXT', 'Suffix text for title wrapping');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPHOMETEXT', 'Show home text');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESHOMETEXTWARNING','When wrapping the hometext, HTML markups elements can become incomplete. Be carefull !');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESMAXHOMETEXTLENGTH', 'Max. lenght of each home text (0 no limit)');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESHOMETEXTWRAPTEXT','Suffix text for home text wrapping'); 
-define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPSPLITCHAR', 'Split character for story info');
+
+define('_NEWS_CONTENTTYPE_NEWSARTICLESBASICINFO', 'Basic information');
 define('_NEWS_CONTENTTYPE_NEWSARTICLESBY', 'by');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESON', 'on');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESREADS', '%count% Reads');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESREAD', '1 Read');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESREADMORE', 'read more');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESCOMMENTS', '%count% Comments');
 define('_NEWS_CONTENTTYPE_NEWSARTICLESCOMMENT', '1 Comment');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPUNAME','Show the user name');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPDATE', 'Show the article creation date');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESCOMMENTS', '%count% Comments');
 define('_NEWS_CONTENTTYPE_NEWSARTICLESDATEFORMAT', 'Formatting of the date');
 define('_NEWS_CONTENTTYPE_NEWSARTICLESDATEFORMATLINK', '<a href="http://www.php.net/manual/function.strftime.php" target="_new">Description for strftime</a>');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPREADS', 'Show the number of reads');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESDESCR', 'Shows a specific number of news articles from one or all the categories available');
 define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPCOMMENTS', 'Show the number of comments');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPDATE', 'Show the article creation date');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPHOMETEXT', 'Show home text');
 define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPNEWIMAGE', 'Display an image for recent story titles');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPREADS', 'Show the number of reads');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPSPLITCHAR', 'Split character for story info');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESDISPUNAME','Show the user name');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESEMPTYMSG', 'No recent news articles found.');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESEXTRAINFO', 'Extra information');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESFRONTPAGE', 'Front page');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESHOMETEXTWARNING','When wrapping the hometext, incomplete HTML markups elements will be made complete again by the truncatehtml plugin.');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESHOMETEXTWRAPTEXT','Suffix text for home text wrapping'); 
+define('_NEWS_CONTENTTYPE_NEWSARTICLESLIMIT', 'Number of articles');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESLINKTOSUBMIT','Display a link to submit a new article');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESMAXDAYS', 'Days limit (0 no limit)');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESMAXHOMETEXTLENGTH', 'Max. lenght of each home text (0 no limit)');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESMAXTITLELENGTH', 'Max. lenght of each title (0 no limit)');
 define('_NEWS_CONTENTTYPE_NEWSARTICLESNEWIMAGELIMIT', 'How many days is a story recent');
 define('_NEWS_CONTENTTYPE_NEWSARTICLESNEWIMAGESET', 'The image set for the new image (pnimg plugin)');
 define('_NEWS_CONTENTTYPE_NEWSARTICLESNEWIMAGESRC', 'The new image name from the set (pnimg plugin)');
-define('_NEWS_CONTENTTYPE_NEWSARTICLESLINKTOSUBMIT','Display a link to submit a new article');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESNONFRONTPAGE', 'Non front page');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESOFTHECAT', 'News articles list of the "%category%" category');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESON', 'on');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESORDER', 'Articles sorted by');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESREAD', '1 Read');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESREADMORE', 'read more');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESREADS', '%count% Reads');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESSHOW', 'Show');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESTITLE', 'Recent News Articles');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESTITLEWRAPTEXT', 'Suffix text for title wrapping');
+define('_NEWS_CONTENTTYPE_NEWSARTICLESWIDGETTITLE', 'Widget title');
+
+define('_NEWS_CONTENTTYPE_ORDERBYCOUNTER', 'Number of reads');
+define('_NEWS_CONTENTTYPE_ORDERBYNEWSSETTING', 'News module setting');
+
+define('_NEWS_CONTENTTYPE_PENDING', 'Pending');
+define('_NEWS_CONTENTTYPE_PUBLISHED', 'Published');
+define('_NEWS_CONTENTTYPE_PUBLISHEDSTATUS', 'Status');
+
+define('_NEWS_CONTENTTYPE_REJECTED', 'Rejected');
+
+define('_NEWS_CONTENTTYPE_UNKNOWN', 'Unknown');
