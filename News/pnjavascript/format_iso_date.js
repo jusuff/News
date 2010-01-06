098bec6c73c8a142a3ff9b2a26ea4c2484dc332d
Erik Spaan <erik.spaan@gmail.com>
2010-01-06 22:01:31 +0000

    Fix in calendar_date_select format iso date.

--- News/pnjavascript/format_iso_date.js
+++ News/pnjavascript/format_iso_date.js
@@ -10,16 +10,19 @@ Date.prototype.toFormattedString = function(include_time) {
 
 Date.parseFormattedString = function (string) {
 
-    var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" + 
-        "( ([0-9]{1,2}):([0-9]{2})?" +
-        "?)?)?)?"; 
-
+// Changed according to http://code.google.com/p/calendardateselect/issues/detail?id=174
+// Ticket 174: Date.parseFormattedString for formate_iso_date.js is broken
+// Function also at http://delete.me.uk/2005/03/iso8601.html
+//    var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" + 
+//        "( ([0-9]{1,2}):([0-9]{2})?" +
+//        "?)?)?)?"; 
+    var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})( ([0-9]{1,2}):([0-9]{2})?)?)?)?";
     var d = string.match(new RegExp(regexp, "i"));
     if (d === null) {
         return Date.parse(string); // at least give javascript a crack at it.
     }
     var offset = 0; 
-    var date = new Date(d[1], 0, 1); 
+    var date = new Date(d[1], 0, 1);
     if (d[3]) {
         date.setMonth(d[3] - 1);
     } 
@@ -32,15 +35,20 @@ Date.parseFormattedString = function (string) {
     if (d[8]) {
         date.setMinutes(d[8]);
     } 
-    if (d[0]) {
-        date.setSeconds(d[0]);
+    if (d[10]) {
+        date.setSeconds(d[10]);
     } 
-    if (d[2]) {
-        date.setMilliseconds(Number("0." + d[2]));
+/* milliseconds and timezone not used !
+    if (d[12]) {
+        date.setMilliseconds(Number("0." + d[12]));
     } 
-    if (d[4]) {
-        offset = (Number(d[6])) + Number(d[8]);
-        offset = ((d[5] == '-') ? 1 : -1); 
+    if (d[14]) {
+        offset = (Number(d[16])) + Number(d[18]);
+        offset = ((d[15] == '-') ? 1 : -1); 
     } 
+    offset -= date.getTimezoneOffset();
+    time = (Number(date) + (offset * 60 * 1000));
+    date.setTime(Number(time));
+*/
     return date; 
 };
