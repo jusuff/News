0a3cbd518f1c7507881e1ed256ae1f924b80f904
Erik Spaan <erik.spaan@gmail.com>
2010-05-06 11:26:01 +0000

    fixes #217, greek language for datepicker

--- News/pnjavascript/datepicker-locale.js
+++ News/pnjavascript/datepicker-locale.js
@@ -119,3 +119,26 @@ Control.DatePicker.Locale['pl_PL'] = {
 	language: 'pl'
 };
 
+// Greek (el_GR)
+// tfotis on http://code.zikula.org/news/ticket/217
+Control.DatePicker.Language['el'] = {
+	months: ['Ιανουάριος','Φεβρουάριος','Μάρτιος','Απρίλιος','Μαϊος','Ιούνιος','Ιούλιος','Αύγουστος','Σεπτέμβριος','Οκτώβριος','Νοέμβριος','Δεκέμβριος'], 
+	days: ['Κυρ', 'Δευ', 'Τρί', 'Τετ', 'Πέμ', 'Παρ', 'Σάβ'], 
+	strings: {
+		'Now': 'Τώρα', 
+		'Today': 'Σήμερα', 
+		'Time': 'Ώρα', 
+		'Exact minutes': 'ακριβή λεπτά', 
+		'Select Date and Time': 'Επιλέξτε αυτή την ημερομηνία και ώρα', 
+		'Open calendar': 'Άνοιγμα ημερολόγιου'
+	}
+};
+
+Control.DatePicker.Locale['el_GR'] = {
+	dateTimeFormat: 'yyyy-MM-dd HH:mm:ss', 
+	dateFormat: 'yyyy-MM-dd', 
+	firstWeekDay: 1, 
+	weekend: [0,6], 
+	language: 'el'
+};
+
